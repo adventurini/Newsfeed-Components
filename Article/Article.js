97da77c39381da99c45dd6b32d416c189138c95a
Anthony Venturini <anthonydventurini@gmail.com>
2019-01-30 13:15:28 -0800

    update Article.js file to expand domElement, which expands the articles on a click event

--- Article/Article.js
+++ Article/Article.js
@@ -3,18 +3,18 @@
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
+    this.domElement= domElement;
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+    this.expandButton.textContent = 'expand';
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
+    this.expandButton.addEventListener('click', ()=> this.expandArticle());
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-
+    this.domElement.classList.toggle('article-open');
   }
 }
 
@@ -26,4 +26,33 @@ class Article {
 
 */
 
-let articles;
+let articles= document.querySelectorAll('.article');
+articles.forEach(domElement => new Article(domElement));
+
+// class Panel {
+//   // constructor
+//   constructor(panel) {
+//     // panel is the individual DOM panel
+//     this.panelButtons = panel.querySelector('.panel-buttons');
+//     this.panelBtnOpen = panel.querySelector('.panel-btn-open');
+//     this.panelBtnClose = panel.querySelector('.panel-btn-close');
+//     this.panelContent = panel.querySelector('.panel-content');
+    
+//     //this.panelButtons.addEventListener('click', this.togglePanel.bind(this));
+//     this.panelButtons.addEventListener('click', () => this.togglePanel());
+    
+//   }
+//   // methods
+//   togglePanel() {
+//     console.log(this.panelBtnOpen);
+//     this.panelBtnOpen.classList.toggle('hide-btn');
+//     this.panelBtnClose.classList.toggle('hide-btn');
+//     this.panelContent.classList.toggle('toggle-on');
+//   }
+// }
+
+// const panels = document.querySelectorAll('.panel');
+// panels.forEach( panel => new Panel(panel));
+
+
+// // DOM panels -> loop (forEach) -> forEach will iterate over each DOM panel individually -> create a new Panel class -> creates a new panel object -> returned back to the dom
